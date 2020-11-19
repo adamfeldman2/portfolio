2a13f9364444ee7c773701afe8a2816d9d5aed12
adamfeldman2 <feldman238@gmail.com>
2020-11-19 18:34:57 -0500

    More cors playground stuff

--- src/pages/Playground.js
+++ src/pages/Playground.js
@@ -1,47 +1,44 @@
 import React, { useState } from "react";
 
 import axios from "axios";
-import { v4 as uuidv4 } from "uuid";
 
 const Playground = () => {
-  const [url, setUrl] = useState("https://");
+  const [url, setUrl] = useState("https://randomuser.me/api/");
 
   const [method, setMethod] = useState("GET");
 
-  const [params, setParams] = useState([
-    { key: "thing", value: "asd", id: uuidv4() },
-  ]);
+  const [params, setParams] = useState('{ "gender": "male", "results": 5 }');
+
+  const [headers, setHeaders] = useState(
+    '{ "Content-Type": "application/json" }'
+  );
+
+  const [data, setData] = useState('{ "thing": "hello world" }');
 
   const onSubmit = async (e) => {
     e.preventDefault();
-    console.log("submit");
+    console.log("Submit info:", {
+      url,
+      method,
+      params: JSON.parse(params),
+      headers: JSON.parse(headers),
+      data: JSON.parse(data),
+    });
     try {
       const res = await axios({
-        method: "POST",
+        method,
 
-        url:
-          "https://app.epiloguewills.com/api/external/v1/questionnaire_link?app_id=1605825206",
+        url,
 
-        headers: { "x-api-key": "TyhM4qt6zpq15lh5wH4RKHK2gxhi4kRX" },
+        headers: JSON.parse(headers),
 
-        // params: { app_id: "1605805123" },
+        params: JSON.parse(params),
 
-        data: {
-          email: "feldman238@gmail.com",
-          province: "Ontario",
-          hasPartner: "no",
-          hasChildren: "yes",
-          familyDisability: "yes",
-          childrenOtherRelationship: "no",
-          firstName: "Orlando",
-          middleName: "Forest",
-          lastName: "Schumacher",
-        },
+        data: JSON.parse(data),
       });
 
       console.log("res.status🎃:", res.status);
       console.log("res.data 🇨🇦:", res.data);
-      console.log("res.headers😈:", res.headers);
     } catch (error) {
       console.log("ERROR.status:", error.response.status);
       console.log("ERROR.response:", error.response.data);
@@ -50,11 +47,12 @@ const Playground = () => {
 
   return (
     <div className="pages-playground wrapper">
-      <form onSubmit={onSubmit}>
+      <form onSubmit={onSubmit} style={{ marginBottom: "200px" }}>
         <div>
           <label>
             URL:
             <input
+              style={{ width: "100%", marginTop: "4px", padding: "5px" }}
               type="text"
               value={url}
               onChange={(e) => setUrl(e.target.value)}
@@ -62,10 +60,19 @@ const Playground = () => {
           </label>
         </div>
 
-        <div>
+        <div style={{ marginTop: "22px" }}>
           <label>
             Method:
-            <select value={method} onChange={(e) => setMethod(e.target.value)}>
+            <select
+              style={{
+                width: "100%",
+                marginTop: "4px",
+                height: "40px",
+                padding: "5px",
+              }}
+              value={method}
+              onChange={(e) => setMethod(e.target.value)}
+            >
               <option value="GET">GET</option>
               <option value="POST">POST</option>
               <option value="PUT">PUT</option>
@@ -75,36 +82,56 @@ const Playground = () => {
           </label>
         </div>
 
-        <div>
-          {params.map((p, i) => {
-            return (
-              <div>
-                <input
-                  key={p.id}
-                  type="text"
-                  value={p.key}
-                  onChange={(e) => setParams([e.target.value])}
-                />
-                :
-                <input
-                  key={p.id}
-                  type="text"
-                  value={p.value}
-                  onChange={(e) => setParams([e.target.value])}
-                />
-              </div>
-            );
-          })}
-          <button
-            onClick={() => {
-              setParams([...params, { key: "", value: "", id: uuidv4() }]);
-            }}
-          >
-            Add Param +
-          </button>
+        <div style={{ marginTop: "22px" }}>
+          <label>
+            Params:
+            <textarea
+              style={{ width: "100%", marginTop: "4px", padding: "7px" }}
+              rows={3}
+              cols={60}
+              value={params}
+              onChange={(e) => setParams(e.target.value)}
+            />
+          </label>
         </div>
 
-        <button type="submit">Submit</button>
+        <div style={{ marginTop: "22px" }}>
+          <label>
+            Headers:
+            <textarea
+              style={{ width: "100%", marginTop: "4px", padding: "7px" }}
+              rows={3}
+              cols={60}
+              value={headers}
+              onChange={(e) => setHeaders(e.target.value)}
+            />
+          </label>
+        </div>
+
+        <div style={{ marginTop: "22px" }}>
+          <label>
+            Body Data:
+            <textarea
+              style={{ width: "100%", marginTop: "4px", padding: "7px" }}
+              rows={4}
+              cols={60}
+              value={data}
+              onChange={(e) => setData(e.target.value)}
+            />
+          </label>
+        </div>
+
+        <button
+          style={{
+            height: "40px",
+            cursor: "pointer",
+            width: "100%",
+            marginTop: "22px",
+          }}
+          type="submit"
+        >
+          Submit
+        </button>
       </form>
     </div>
   );
